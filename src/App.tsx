8a8ac0f0ed931b9bfc130e907774d11731cfeef9
C-Sharpener <56298258+C-Sharpener@users.noreply.github.com>
2022-02-08 00:09:04 +0900

    Custom Hookを導入した

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./styles.css";
 import { UserCard } from "./components/UserCard";
-import axios from "axios";
-import { User } from "./types/api/user";
-import { useState } from "react";
-import { UserProfile } from "./types/UserProfile";
+import { useAllUsers } from "./hooks/useAllUsers";
+// import axios from "axios";
+// import { User } from "./types/api/user";
+// import { useState } from "react";
+// import { UserProfile } from "./types/UserProfile";
 
 // const user = {
 //   id: 1,
@@ -13,36 +14,37 @@ import { UserProfile } from "./types/UserProfile";
 // };
 
 export default function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  // const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(false);
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
 
-  const onClickFetchUser = () => {
-    setLoading(true);
-    setError(false);
-
-    // axios.get("https://jsonplaceholder.typicode.com/users");
-    // axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users");
-    axios
-      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
-      // .get<Array<User>>("https://jsonplaceholder.typicode.com/usersxxxx")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setUserProfiles(data);
-        // });
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  // const onClickFetchUser = () => {
+  // setLoading(true);
+  // setError(false);
+  // // axios.get("https://jsonplaceholder.typicode.com/users");
+  // // axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users");
+  // axios
+  //   .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+  //   // .get<Array<User>>("https://jsonplaceholder.typicode.com/usersxxxx")
+  //   .then((res) => {
+  //     const data = res.data.map((user) => ({
+  //       id: user.id,
+  //       name: `${user.name}(${user.username})`,
+  //       email: user.email,
+  //       address: `${user.address.city}${user.address.suite}${user.address.street}`
+  //     }));
+  //     setUserProfiles(data);
+  //     // });
+  //   })
+  //   .catch(() => {
+  //     setError(true);
+  //   })
+  //   .finally(() => {
+  //     setLoading(false);
+  //   });
+  // };
+  const onClickFetchUser = () => getUsers();
 
   return (
     <div className="App">
@@ -56,7 +58,7 @@ export default function App() {
         <>
           {userProfiles.map((user) => (
             // <UserCard user={user} />
-            <UserCard key={useState.id} user={user} />
+            <UserCard key={user.id} user={user} />
           ))}
         </>
       )}
